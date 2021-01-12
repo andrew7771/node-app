c0d35e91ac1334b6c1bd80d3caa5d80586310226
Andrii Turianskyi <aturyansky33@gmail.com>
2021-01-12 13:20:45 +0200

    minor fixes

--- src/routes.ts
+++ src/routes.ts
@@ -4,10 +4,10 @@ import { TableInitializer } from './app/models/table-initializer';
 export const router = Router();
 
 const initializer = new TableInitializer();
-
-router.get('/initialize', initializer.create);
 router.get('/', (req, res) => {
     res.send('Server is up an running');
 });
+
+router.get('/initialize', initializer.create);
 
 
